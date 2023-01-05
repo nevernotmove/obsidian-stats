8f790a54d7c7e30361d79d9b781fa446d9c0fec1
nevernotmove <nevernotmove@gmail.com>
2023-01-05 21:54:02 +0100

    Add script to deploy to GitHub pages

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# On Windows (with git installed) you can set the npm shell like so:
+# npm config set script-shell "C:\\Program Files\\git\\bin\\bash.exe"
+# That way you can run this script via the npm script
+# To unset it do this:
+# npm config delete script-shell
+
+# Abort on errors
+set -e
+
+# Build
+npm run build
+
+# Navigate into the build output directory
+cd dist
+
+# Place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:nevernotmove/plugin-stats.git main:gh-pages
+
+cd -
