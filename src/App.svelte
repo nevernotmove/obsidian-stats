3945297e4208fccded7887a468329fbd41783815
nevernotmove <nevernotmove@gmail.com>
2023-01-07 18:09:11 +0100

    #3 Split app into ChartView and Nav

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,16 @@
 <script lang="ts">
-    import {Chart, registerables} from 'chart.js';
-    import json from '../public/downloads.json';
-    import json2 from '../public/community-plugin-stats.json';
+    import Nav from './Nav.svelte';
     import {onMount} from 'svelte';
-    import pluginDownloadsOverTime from './pluginDownloadsOverTime';
-    import mostDownloadedPlugins from './mostDownloadedPlugins';
-
-    // Bundling fails if missing
-    Chart.register(...registerables);
-
-    // Set default colors
-    Chart.defaults.backgroundColor = '#1E1E1E';
-    Chart.defaults.borderColor = 'rgb(139, 108, 239, 1)';
-    Chart.defaults.color = '#B4B4B4';
+    import ChartView from './ChartView.svelte';
 
     onMount(() => {
-        pluginDownloadsOverTime(json)
+        //pluginDownloadsOverTime(json)
         //mostDownloadedPlugins(json2);
     })
 </script>
 
 <main>
-    <div class="chart-container">
-        <canvas id="chart"></canvas>
-    </div>
+    <Nav/>
+    <ChartView/>
 </main>
 
-<style>
-    .chart-container {
-        position: relative;
-        height: 100vh;
-        width: 100vw;
-    }
-
-    #chart {
-        margin: 5%;
-    }
-</style>
---
+++ src/ChartView.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import {Chart, registerables} from 'chart.js';
+    import json from '../public/downloads.json';
+    import json2 from '../public/community-plugin-stats.json';
+    import {onMount} from 'svelte';
+    import pluginDownloadsOverTime from './pluginDownloadsOverTime';
+    import mostDownloadedPlugins from './mostDownloadedPlugins';
+
+    // Bundling fails if missing
+    Chart.register(...registerables);
+
+    // Set default colors
+    Chart.defaults.backgroundColor = '#1E1E1E';
+    Chart.defaults.borderColor = 'rgb(139, 108, 239, 1)';
+    Chart.defaults.color = '#B4B4B4';
+
+    onMount(() => {
+        pluginDownloadsOverTime(json)
+        //mostDownloadedPlugins(json2);
+    })
+</script>
+
+<div class="chart-container">
+    <canvas id="chart"></canvas>
+</div>
+
+<style>
+    .chart-container {
+        position: relative;
+        height: 100vh;
+        width: 100vw;
+    }
+
+    #chart {
+        margin: 5%;
+    }
+</style>
