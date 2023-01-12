626f957ed9198ec0bbc6c509a84f3e42991e1c20
nevernotmove <nevernotmove@gmail.com>
2023-01-12 21:37:30 +0100

    #21 Be able to select search suggestion with arrow keys and enter

--- src/chart/PluginDownloads/SearchBar.svelte
+++ src/chart/PluginDownloads/SearchBar.svelte
@@ -7,14 +7,17 @@
     export let maxSuggestions: number = 3;
 
     let suggestions: string[];
-    resetSuggestions();
+    let activeSuggestion: number;
     $: showSuggestions = suggestions.length > 0;
+    resetSuggestions();
 
     function resetSuggestions() {
         suggestions = [];
+        activeSuggestion = -1;
     }
 
     function onSubmit() {
+        console.log("onSubmit");
         searchText = searchText.trim();
         if (searchText === '') return;
         let pluginExists = false;
@@ -30,6 +33,7 @@
     }
 
     function onInput() {
+        console.log("onInput");
         error = false;
         if (searchText === '') {
             resetSuggestions();
@@ -64,26 +68,59 @@
     }
 
     function onSelect(e) {
+        console.log("onSelect");
         const id = e.target.id;
         searchText = suggestions[id];
         onSubmit();
     }
+    
+    function onKeyDown(e) {
+        console.log("onKeyDown");
+        if (e.key === 'ArrowDown' || e.key === 'Down') {
+            const list = document.getElementById("suggestions").children[0].children as HTMLElement;
+            if (activeSuggestion < maxSuggestions - 1) activeSuggestion++;
+            const el = list[activeSuggestion] as HTMLElement;
+            console.log(activeSuggestion, el);
+            el.focus();
+        }
+        else if (e.key === 'ArrowUp' || e.key === 'Up') {
+            const list = document.getElementById("suggestions").children[0].children as HTMLElement;
+            if (activeSuggestion > 0) {
+                activeSuggestion--;
+                const el = list[activeSuggestion] as HTMLElement;
+                console.log(activeSuggestion, el);
+                el.focus();
+            }
+            else {
+                const searchBar = document.getElementById("searchbar") as HTMLElement;
+                console.log(searchBar);
+                searchBar.focus();
+            }
+        }
+        else if (e.key === "Enter") {
+            console.log("jo enter");
+            searchText = suggestions[activeSuggestion];
+            onSubmit();
+        }
+    }
 </script>
 
-<form on:submit|preventDefault={onSubmit}>
+<!--TODO Add back to input attributes-->
+<!-- on:focusout={resetSuggestions} -->
+<form on:submit|preventDefault={onSubmit} on:keydown={e => onKeyDown(e)}>
     <input 
+            id="searchbar"
             autofocus type="text"
             placeholder="Enter plugin name"
             bind:value={searchText}
             class:error={error}
-            on:input={onInput} 
-            on:focusout={resetSuggestions}
-            on:focus={onInput}>
+            on:input={e => onInput(e)} 
+            on:focus={e => onInput(e)}>
     {#if showSuggestions}
-        <div class="suggestions">
+        <div id="suggestions">
             <ul>
                 {#each suggestions as s, id}
-                    <li id={id} on:click={(e) => onSelect(e)}>{s}</li>
+                    <li id={id} tabindex="1" on:click={(e) => onSelect(e)}>{s}</li>
                 {/each}
             </ul>
         </div>
@@ -109,7 +146,7 @@
         background-color: var(--color-error);
     }
 
-    .suggestions {
+    #suggestions {
         position: relative;
     }
 
@@ -132,8 +169,12 @@
         background-color: var(--color2);
         cursor: pointer;
     }
+    
+    li:focus {
+        background-color: yellow;
+    }
 
-    li:hover {
+    li:hover, li:focus {
         background-color: var(--color4);
     }
 </style>
