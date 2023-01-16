1126a2ff18660f94673b04df6d48bc2c7820c3d6
nevernotmove <nevernotmove@gmail.com>
2023-01-16 01:48:07 +0100

    #34 Only animate logo on start page

--- src/nav/Logo.svelte
+++ src/nav/Logo.svelte
@@ -6,19 +6,19 @@
 
     export let big = false;
     let start = false;
-    
-    let strokeWidth = "25px";
+
+    let strokeWidth: string = "12px";
     if (big) {
         strokeWidth = "10px";
     }
-    
+
     onMount(() => start = true);
 </script>
 
 <span>
     <Link to="/">
         <svg viewBox="0 0 2160 2160">
-            {#if start}
+            {#if start && big}
                 <path transition:draw|local="{{duration: 4000, delay: 200, easing: quintOut}}"
                       d="m1355 1988 98-187 192-1172-358-222-501 398 569 1183Z"
                       fill="var(--logo-fill-color)"
@@ -49,6 +49,37 @@
                       stroke-width={strokeWidth}
                       stroke-linejoin="round"
                 />
+            {:else if start}
+                <path 
+                      d="m1355 1988 98-187 192-1172-358-222-501 398 569 1183Z"
+                      fill="var(--logo-fill-color)"
+                      stroke="var(--logo-line-color)"
+                      stroke-width={strokeWidth}
+                      stroke-linejoin="round"
+                                />
+                <path 
+                      d="m1645 629-340-487-18 265 358 222Z"
+                      fill="var(--logo-fill-color)"
+                      stroke="var(--logo-line-color)"
+                      stroke-width={strokeWidth}
+                      stroke-linejoin="round"
+                />
+        
+                <path 
+                      d="m726 469 60 336 501-398 18-265-579 327ZM1355 1988l-520-187-49-996 569 1183Z"
+                      fill="var(--logo-fill-color)"
+                      stroke="var(--logo-line-color)"
+                      stroke-width={strokeWidth}
+                      stroke-linejoin="round"
+                />
+        
+                <path
+                      d="M726 469 484 985l351 816-49-996-60-336Z"
+                      fill="var(--logo-fill-color)"
+                      stroke="var(--logo-line-color)"
+                      stroke-width={strokeWidth}
+                      stroke-linejoin="round"
+                />
             {/if}
         </svg>
     </Link>
@@ -62,7 +93,7 @@
     span:hover, span:focus {
         filter: invert(27%) sepia(51%) saturate(2878%) hue-rotate(346deg) brightness(104%) contrast(97%);
     }
-    
+
     svg {
         height: 100%;
     }
